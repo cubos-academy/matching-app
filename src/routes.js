d6623f833216ece36df878a676d7181a26300932
Nicolas 'eowfenth' Deçordi dos Reis <eowfenth.services@gmail.com>
2020-04-27 16:52:31 -0300

    feat: add Routes

---
+++ src/routes.js
@@ -0,0 +1,47 @@
+const { Router } = require("express");
+
+const User = require("./user/controller");
+const { checkAccountStatus, encryptPassword } = require("./user/middleware");
+
+const Session = require("./session/controller");
+const { checkAuthorization } = require("./session/middleware");
+
+const routes = Router();
+
+routes.get("/hello", (req, res) => {
+  res.json({ error: null, data: { message: "Hello, World!!" } });
+});
+
+routes.post("/auth/", Session.auth);
+routes.post("/auth/google", () => {});
+routes.post("/auth/google", () => {});
+
+routes.get(
+  "/users/me",
+  checkAuthorization,
+  checkAccountStatus,
+  User.getUserProfile
+);
+routes.get(
+  "/users/:id",
+  checkAuthorization,
+  checkAccountStatus,
+  User.getProfile
+);
+routes.post("/users/", encryptPassword, User.create);
+routes.put(
+  "/users/me",
+  checkAuthorization,
+  checkAccountStatus,
+  encryptPassword,
+  User.update
+);
+
+routes.put(
+  "/users/me/disable",
+  checkAuthorization,
+  checkAccountStatus,
+  User.disable
+);
+
+module.exports = routes;
