75e4b0b3f6f483f4a109fc4a12cf8f1508e44949
Nicolas 'eowfenth' Deçordi dos Reis <eowfenth.services@gmail.com>
2020-04-27 16:51:40 -0300

    feat: add User middleware

---
+++ src/user/middleware.js
@@ -0,0 +1,33 @@
+const bcrypt = require("bcryptjs");
+const User = require("./model");
+
+const encryptPassword = async (req, res, next) => {
+  const { password = null } = req.body;
+
+  if (password) {
+    const password_hash = await bcrypt.hash(password, 10);
+
+    req.body.password_hash = password_hash;
+  }
+
+  return next();
+};
+
+const checkAccountStatus = async (req, res, next) => {
+  const { auth_user_id } = req.body;
+
+  const user = await User.getOne(auth_user_id);
+
+  if (user.deleted_at) {
+    return res.json({
+      error: 403,
+      data: {
+        message: "Your user account is disabled",
+      },
+    });
+  }
+
+  return next();
+};
+
+module.exports = { checkAccountStatus, encryptPassword };
