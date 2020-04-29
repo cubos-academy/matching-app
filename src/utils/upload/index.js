8e5bb426fd42faa63ed65b5235fe59ce203c3376
Nícolas 'nohaleh' Deçordi <eowfenth.services@gmail.com>
2020-04-29 12:17:04 -0300

    feat: add file filter to only allow image some image types to be upload

--- src/utils/upload/index.js
+++ src/utils/upload/index.js
@@ -4,12 +4,30 @@ const multerS3 = require('multer-s3');
 const path = require("path");
 
 const fileCheck = (req, file, next) => {
-    if (file.originalName.match('/\.(jpg|jpeg|png)$/')) {
-        return next(new Error('Only image files are allowed!'), false)
+    const ext = path.extname(file.originalname);
+    if(ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
+        return next(new Error('FILE_EXTENSION_NOT_ALLOWED'));
     }
 
     return next(null, true);
 };
+
+const s3 = new AWS.S3({
+    accessKeyId: "",
+    secretAccessKey: "",
+});
+
+const s3fileUpload = multer({
+    storage: multerS3({
+        s3: s3,
+        acl: 'public-read',
+        bucket: 'bucket-name',
+        metadata: (req, file, cb) => { cb(null, { fieldName: file.fieldname })},
+        key: (req, file, cb) => { cb(null, Date.now().toString() + '-' + file.originalName) },
+    }),
+    limits: { fileSize: 5*1024*1024 }, // 5mb
+    fileFilter: fileCheck,
+});
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -25,6 +43,7 @@ const storage = multer.diskStorage({
 const Uploader = multer({
     storage,
     limits: { fileSize: 5*1024*1024 },
+    fileFilter: fileCheck,
 }).single('file');
 
 const fileUpload = (req, res, next) => {
@@ -35,18 +54,25 @@ const fileUpload = (req, res, next) => {
                 return res.json({
                     error: 422,
                     data: {
-                        message: "The file is too large."
+                        message: "The file is too large"
                     },
                 });
             }
         } else if (err) {
-        console.error(err);
-        return res.json({
-            error: 503,
-            data: {
-                message: "Internal Error",
-            },
-        });
+            console.error(err);
+            if (err.message === "FILE_EXTENSION_NOT_ALLOWED") {
+                return res.json({
+                    error: 422,
+                    message: "File extension not supported",
+                });
+            }
+
+            return res.json({
+                error: 503,
+                data: {
+                    message: "Internal Error",
+                },
+            });
         }
 
         next();
