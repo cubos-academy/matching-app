ecb9c0963ffd5c0abdf51ac27effdc564ea2c3b2
Nícolas 'nohaleh' Deçordi <eowfenth.services@gmail.com>
2020-04-29 13:22:33 -0300

    refactor: set aws variables in .env

--- src/utils/upload/index.js
+++ src/utils/upload/index.js
@@ -21,7 +21,7 @@ const S3Storage = {
     storage: multerS3({
         s3: s3,
         acl: 'public-read',
-        bucket: 'matching-app-bucket',
+        bucket: process.env.AWS_S3_BUCKETNAME,
         metadata: (req, file, cb) => { cb(null, { fieldName: file.fieldname })},
         key: (req, file, cb) => { 
             const uniquePrefix = Date.now() + '-' + Math.round(Math.random() * 1E9);
