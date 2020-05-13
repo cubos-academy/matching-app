2d4ac3d639ca9466e85e379bb607a1786851d0fc
Nícolas 'nohaleh' Deçordi <eowfenth.services@gmail.com>
2020-05-13 13:49:07 -0300

    style: adjust code style of middlewares

--- src/session/middleware.js
+++ src/session/middleware.js
@@ -1,30 +1,30 @@
 const jwt = require('jsonwebtoken');
 
 const checkAuthorization = async (req, res, next) => {
-  const { authorization } = req.headers;
+	const { authorization } = req.headers;
 
-  if (!authorization) {
-    return res.status(401).json({
-      error: 401,
-      data: {
-        message: 'Unauthorized',
-      },
-    });
-  }
+	if (!authorization) {
+		return res.status(401).json({
+			error: 401,
+			data: {
+				message: 'Unauthorized',
+			},
+		});
+	}
 
-  const [bearer, token] = authorization.split(' ');
-  try {
-    const verification = await jwt.verify(token, process.env.JWT_SECRET);
-    req.auth_user_id = verification.id;
-  } catch (err) {
-    res.status(401).json({
-      error: 401,
-      data: {
-        message: 'Unauthorized',
-      },
-    });
-  }
-  return next();
+	const [bearer, token] = authorization.split(' ');
+	try {
+		const verification = await jwt.verify(token, process.env.JWT_SECRET);
+		req.auth_user_id = verification.id;
+	} catch (err) {
+		res.status(401).json({
+			error: 401,
+			data: {
+				message: 'Unauthorized',
+			},
+		});
+	}
+	return next();
 };
 
 module.exports = { checkAuthorization };
--- src/user/middleware.js
+++ src/user/middleware.js
@@ -1,40 +1,42 @@
+/* eslint-disable no-else-return */
+
 const bcrypt = require('bcryptjs');
 const User = require('./model');
 
 const encryptPassword = async (req, res, next) => {
-  const { password = null } = req.body;
+	const { password = null } = req.body;
 
-  if (password) {
-    const password_hash = await bcrypt.hash(password, 10);
+	if (password) {
+		const password_hash = await bcrypt.hash(password, 10);
 
-    req.body.password_hash = password_hash;
-  }
+		req.body.password_hash = password_hash;
+	}
 
-  return next();
+	return next();
 };
 
 const checkAccountStatus = async (req, res, next) => {
-  const { auth_user_id } = req;
+	const { auth_user_id } = req;
 
-  const user = await User.getOne(auth_user_id);
+	const user = await User.getOne(auth_user_id);
 
-  if (!user) {
-    return res.json({
-      error: 403,
-      data: {
-        message: "Forbidden",
-      },
-    });
-  } else if (user && user.deleted_at) {
-    return res.json({
-      error: 403,
-      data: {
-        message: 'Your user account is disabled',
-      },
-    });
-  }
+	if (!user) {
+		return res.json({
+			error: 403,
+			data: {
+				message: 'Forbidden',
+			},
+		});
+	} else if (user && user.deleted_at) {
+		return res.json({
+			error: 403,
+			data: {
+				message: 'Your user account is disabled',
+			},
+		});
+	}
 
-  return next();
+	return next();
 };
 
 module.exports = { checkAccountStatus, encryptPassword };
