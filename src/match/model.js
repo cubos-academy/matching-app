6283f8b8451493feeb9e5bbb9a877d27c7e1f9b7
Nícolas 'nohaleh' Deçordi <eowfenth.services@gmail.com>
2020-06-01 17:57:07 -0300

    feat: add models methods for matches

--- src/match/model.js
+++ src/match/model.js
@@ -10,12 +10,12 @@ const DEFAULT_ERR_RESPONSE = {
 	},
 };
 
-const getOne = async (user_id, match_id) => {
+const getLike = async (user_id, match_id) => {
 	try {
 		const {
 			rows,
 		} = await Pool.query(
-			'SELECT id, email, password_hash FROM users_matches WHERE user_id = $1 AND match_id = $2 AND deleted_at IS NULL',
+			'SELECT * FROM users_matches WHERE (user_id = $1 AND match_id = $2) OR (user_id = $2 AND match_id = $1) AND deleted_at IS NULL',
 			[user_id, match_id],
 		);
 
@@ -34,26 +34,25 @@ const index = async (user_id) => {
 		const {
 			rows,
 		} = await Pool.query(
-			'SELECT * from users_matches WHERE user_id = $1 AND deleted_at IS NULL AND match_liked = TRUE and user_liked = TRUE',
+			'SELECT * from users_matches WHERE (user_id = $1 OR match_id = $1) AND deleted_at IS NULL AND match_liked = TRUE and user_liked = TRUE',
 			[user_id],
 		);
 
 		if (!rows.length) {
-			return null;
+			return {
+				error: null,
+				data: [],
+			};
 		}
 
-		return rows;
+		return {
+			error: null,
+			data: rows,
+		};
 	} catch (err) {
 		return DEFAULT_ERR_RESPONSE;
 	}
 };
-
-// const match = async (user_id, match_id) => {
-// 	try {
-// 	    const match =
-// 		const { rows } = await Pool.query(`UPDATE users_matches SET `, [email]);
-// 	} catch
-// };
 
 const dismatch = async (user_id, match_id) => {
 	try {
@@ -70,4 +69,64 @@ const dismatch = async (user_id, match_id) => {
 	}
 };
 
-module.exports = { dismatch, getOne, index };
+const like = async (user_id, match_id, like_id) => {
+	if (like_id) {
+		try {
+			await Pool.query(
+				`UPDATE users_matches SET match_liked = TRUE WHERE match_id = $1`,
+				[user_id],
+			);
+
+			return {
+				success: true,
+			};
+		} catch (err) {
+			return DEFAULT_ERR_RESPONSE;
+		}
+	}
+
+	try {
+		await Pool.query(
+			`INSERT INTO users_matches (user_id, match_id, user_liked, match_liked) VALUES ($1, $2, TRUE, NULL)`,
+			[user_id, match_id],
+		);
+
+		return {
+			success: true,
+		};
+	} catch (err) {
+		return DEFAULT_ERR_RESPONSE;
+	}
+};
+
+const pass = async (user_id, match_id, like_id) => {
+	if (like_id) {
+		try {
+			await Pool.query(
+				`UPDATE users_matches SET match_liked = FALSE WHERE match_id = $1`,
+				[user_id],
+			);
+
+			return {
+				success: true,
+			};
+		} catch (err) {
+			return DEFAULT_ERR_RESPONSE;
+		}
+	}
+
+	try {
+		await Pool.query(
+			`INSERT INTO users_matches (user_id, match_id, user_liked, match_liked) VALUES ($1, $2, FALSE, NULL)`,
+			[user_id, match_id],
+		);
+
+		return {
+			success: true,
+		};
+	} catch (err) {
+		return DEFAULT_ERR_RESPONSE;
+	}
+};
+
+module.exports = { dismatch, getLike, index, like, pass };
